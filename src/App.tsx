b5c147ebe97a9bcb5e16a8ba6a82abb95a7b0662
Nanda Kalyan <127862357+NandaKalyan@users.noreply.github.com>
2026-08-25 13:53:02 +0530

    Adjust scroll threshold from 0.54 to 0.50

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
 
       targetProgressRef.current = progress;
 
-      setHasScrolled(progress > 0.54);
+      setHasScrolled(progress > 0.50);
 
       setStage(
         progress < 0.34
